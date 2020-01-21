1771505675a55f2b9154e492f9c7b570b6ba791b
Benjamin Huynh <ben.huynh408@gmail.com>
2020-01-21 15:55:02 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Ben Huynh on 1/21/20.
+//  Copyright © 2020 Ben Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
